526816d2efd18c1275c7bd25ae0a7e99dc5f8dbb
Erel Segal-Halevi cron <erelsgl@gmail.com>
2021-12-15 14:56:53 +0200

    update pdf files

--- 09-participatory-budgeting/code/fractional.py
+++ 09-participatory-budgeting/code/fractional.py
@@ -18,10 +18,10 @@ a, b, c, d = allocations
 
 # There are 5 citizens. Their preferences are: ab, ac, ad, bc, a. The total budget is 500 - 100 for each citizen
 donations = [100, 100, 100, 100, 100]
-utilities = [a+b, a+c, a+d, b+c, a]
+utilities = [b+d, a+c, a+d, b+c, a]
 
 sum_of_logs = cvxpy.sum([cvxpy.log(u) for u in utilities])
-positivity_constraints = [v>=0 for v in allocations]
+positivity_constraints = [v >= 0 for v in allocations]
 sum_constraint = [cvxpy.sum(allocations)==sum(donations)]
 
 problem = cvxpy.Problem(
@@ -29,15 +29,16 @@ problem = cvxpy.Problem(
     constraints = positivity_constraints+sum_constraint)
 problem.solve()
 
-utility_values = [u.value/100 for u in utilities]
+utility_values = [u.value for u in utilities]
 print("BUDGET: a={}, b={}, c={}, d={}".format(a.value, b.value, c.value, d.value))
 print("UTILS : {}, {}, {}, {}, {}".format(*utility_values))
 utility_product = functools.reduce(lambda a,b: a*b, utility_values)
 print("PRODUCT: {}".format(utility_product))
 i=0
-print("Citizen {} should donate {} to a and {} to b".format(i,
-    a.value * donations[i] / utilities[i].value,
-    b.value * donations[i] / utilities[i].value
+print("Citizen {} should donate  {} to b and {} to d".format(i,
+    # a.value * donations[i] / utilities[i].value,
+    b.value * donations[i] / utilities[i].value,
+    d.value * donations[i] / utilities[i].value
 ))
 i+=1
 print("Citizen {} should donate {} to a and {} to c".format(i,
